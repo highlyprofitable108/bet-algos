aefe3d204944d198a46c32f5314013f729697dd5
mikefusc <mikefusc@me.com>
2023-04-26 23:03:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,13 +29,24 @@ def load_data():
         fg_data = fg_data.drop(fg_data.index[fg_data['RK'] == 'RK'])
     fg_data = fg_data.rename(columns={'Name': 'name', 'Team': 'team', 'G': 'games', 'PA': 'plate_appearances', 'HR': 'home_runs', 'R': 'runs', 'RBI': 'runs_batted_in', 'SB': 'stolen_bases', 'BB%': 'walk_percentage', 'K%': 'strikeout_percentage', 'ISO': 'isolated_power', 'BABIP': 'batting_average_on_balls_in_play', 'AVG': 'batting_average', 'OBP': 'on_base_percentage', 'SLG': 'slugging_percentage', 'wOBA': 'weighted_on_base_average', 'wRC+': 'weighted_runs_created_plus'})
 
+    print("Baseball-Reference data shape:", br_data.shape)
+    print("Baseball-Reference data columns:", br_data.columns)
+    print("FanGraphs data shape:", fg_data.shape)
+    print("FanGraphs data columns:", fg_data.columns)
+    data = pd.merge(br_data, fg_data, on='name')
+    print("Merged data shape:", data.shape)
+    print("Merged data columns:", data.columns)
+    unmatched_names_br = br_data[~br_data['name'].isin(fg_data['name'])]['name']
+    unmatched_names_fg = fg_data[~fg_data['name'].isin(br_data['name'])]['name']
+    print("Unmatched Baseball-Reference names:", unmatched_names_br)
+    print("Unmatched FanGraphs names:", unmatched_names_fg)
 
     # Match Baseball-Reference player names to FanGraphs player names
     br_data['name'] = br_data['name'].str.replace('[^\w\s]','').str.lower()
     fg_data['name'] = fg_data['Name'].str.replace('[^\w\s]','').str.lower()
 
 	# Merge the data
-    data = pd.merge(br_data, fg_data, on='name')
+    data = pd.merge(br_data, fg_data, on='name', how='outer')
 
     
     return data
