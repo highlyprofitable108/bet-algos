5294e9a7031937c99e8461ffc9d4c80be9e13f87
mikefusc <mikefusc@me.com>
2023-04-26 20:16:18 -0400

    Update run_analysis.py

--- run_analysis.py
+++ run_analysis.py
@@ -1,4 +1,4 @@
-import pandas as pd
+mport pandas as pd
 from models.data_integration import get_combined_data
 from models.lineup_optimizer import generate_optimal_lineup
 
@@ -8,8 +8,15 @@ pricing_data = pd.read_csv('data/pricing.csv')
 # Load data from multiple sources
 data = get_combined_data()
 
-# Generate optimal lineup
-optimal_lineup = generate_optimal_lineup(data, pricing_data)
+# Generate optimal lineups
+optimal_lineups = generate_optimal_lineup(data, pricing_data, num_lineups=10)
 
-# Print the optimal lineup
-print(optimal_lineup)
+# Print the top 10 lineups
+for i, lineup_data in enumerate(optimal_lineups):
+    print(f"Lineup {i+1}:")
+    print(lineup_data['lineup'])
+    print(f"Projected points: {lineup_data['lineup_points']:.2f}")
+    print("Player projected points:")
+    for j, player_points in enumerate(lineup_data['player_points']):
+        print(f"{lineup_data['lineup'][j]}: {player_points:.2f}")
+    print()
